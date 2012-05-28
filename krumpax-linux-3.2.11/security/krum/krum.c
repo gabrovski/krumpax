06a1889c9bb29d0d727777dccf18d099598a26bb
sasho <sashogabrovski@gmail.com>
2012-05-28 18:56:56 -0400

    can now compile in krum

--- krumpax-linux-3.2.11/security/krum/krum.c
+++ krumpax-linux-3.2.11/security/krum/krum.c
@@ -4,12 +4,13 @@
 #include <asm/current.h>
 #include "include/krum.h"
 
-static struct security_operations krum_ops {
+static struct security_operations krum_ops = {
   .name = "krum",
 };
 
 static int __init krum_init(void) {
   int error;
+  printk(KERN_INFO "trying to load krum\n");
 
   error = register_security(&krum_ops);
   if (error) 
@@ -19,3 +20,6 @@ static int __init krum_init(void) {
 }
 
 void cleanup_module() { }
+
+MODULE_LICENSE("GPL");
+security_initcall(krum_init);
