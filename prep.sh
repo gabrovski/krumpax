f9f5ae75ade29302062c177e6002e432ebcf216b
root <root@archy.localdomain>
2012-05-29 21:07:14 -0400

    fixed mkinitcpio bug. stupid stupid stupid

--- prep.sh
+++ prep.sh
@@ -5,5 +5,5 @@ make ;
 make modules_install ;
 cd .. ;
 cp -v krumpax-linux-3.2.11/arch/x86/boot/bzImage /boot/vmlinuz-linux-pax ;
-mkinitcpio -k 3.2.11-pax /boot/initramfs-linux-pax.img ;
+mkinitcpio -k 3.2.11-pax -g /boot/initramfs-linux-pax.img ;
 cp -v krumpax-linux-3.2.11/System.map /boot/System.map-3.2.11-pax ;
