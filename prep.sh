ceeb714dbe6be6542e1d131cd39e911e3da664ea
sasho <sashogabrovski@gmail.com>
2012-05-29 20:01:00 -0400

    updated automated build

--- prep.sh
+++ prep.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cd krumpax-linux-3.2.11 ;
+make ;
+make modules_install ;
+cd .. ;
 cp -v krumpax-linux-3.2.11/arch/x86/boot/bzImage /boot/vmlinuz-linux-pax ;
-mkinitcpio -k 3.2.11-ARCH /boot/initramfs-linux-pax.img ;
-cp -v krumpax-linux-3.2.11/System.map /boot/System.map-3.2.11-ARCH ;
+mkinitcpio -k 3.2.11-pax /boot/initramfs-linux-pax.img ;
+cp -v krumpax-linux-3.2.11/System.map /boot/System.map-3.2.11-pax ;
